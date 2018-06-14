60b50862ee59d66dbd7be61dbfb190ab605651c2
josue <josue.azur@gmail.com>
2018-06-14 06:59:59 +0200

    tpl detail-offre init

---
+++ wp-content/themes/coezion2/details-offres.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Template Name: details-offre
+ */
+    get_header();
+?>
+<div id="fond_bois"></div>
+<div class="wrap">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <?php
+                get_template_part( 'template-parts/post/content', get_post_format() );
+                get_template_part( 'template-parts/offres/details-offre', get_post_format() );
+            ?>
+        </main>
+    </div>
+    <?php //get_sidebar(); ?>
+</div><!-- .wrap -->
+<div id="fond_bois"></div>
+<?php
+get_footer();
--- wp-content/themes/coezion2/template-parts/offres/details-offre.php
+++ wp-content/themes/coezion2/template-parts/offres/details-offre.php
@@ -2,11 +2,9 @@
     <div id="main" class="container">
         <div id="liste-offres">
             <section id="offres">
-
             </section>
             <script>
                 var $ = jQuery;
-                var offres = [];
                 var section = $("section#offres");
 
                 var data_offres = {
@@ -15,14 +13,11 @@
                     "JobTitle": "", 
                     "PostalCode": ""
                 };
-
-                var bloc_offre_begin = '<div id="bloc" class="6u 12u(mobile)">' +
-                        '<div class="iframe-responsive-wrapper" style="height-max: 120px;">' +
-                                '<p style="height: 60px; overflow: hidden; -o-text-overflow: ellipsis; text-overflow: ellipsis; ">' +
-                                    '<h3><center>';
-                var bloc_offre_end = '</center></h3></p></div>';
-
+                
                 $(document).ready(function(){
+                    var params = window.location.search.split("=");
+                    var id = params[1];
+                    
                     $.ajax({
                         url: "http://api.infolor.fr/api/CRM/GetWebAnnouncesByCriteria",
                         type: "POST",
@@ -30,17 +25,17 @@
                         dataType: "json",
                         success: function(response){
                             section.html('');
-
+                            var loffre;
+                            
                             $.each(response, function(index, offre){
-                                var title = JSON.stringify(offre.Title).replace(/"/g, "") + '<br/>';
-                                var date = JSON.stringify(offre.Date).replace(/"/g, "") + '<br/>';
-                                var link = '<center><a href="details-offre?id=' + JSON.stringify(offre.AnnouceId) + '" class="button"> Voir les d&eacute;tails de l\'offre</a></center>';
-                                
-                                section.prepend(bloc_offre_begin + title + date + link + bloc_offre_end);
+                                if(id == JSON.stringify(offre.AnnouceId)){
+                                    loffre = offre;
+                                }
                             });
+                            section.prepend(JSON.stringify(loffre));
                         },
                         error: function(e){
-                            console.log("Erreur: " + e)
+                            console.log("Erreur: ", e);
                         }
                     });
                 });
