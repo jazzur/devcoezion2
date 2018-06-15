36e91ab6c1cacc3e57c7a01250e29550fa0b55a9
josue <josue.azur@gmail.com>
2018-06-15 03:40:18 +0200

    responsive menu

--- wp-content/themes/coezion2/footer.php
+++ wp-content/themes/coezion2/footer.php
@@ -1,9 +1,9 @@
 </div><!-- #content -->
     <footer id="colophon" class="site-footer" role="contentinfo">
-            <div class="wrap">
-                    <?php
-                        get_template_part( 'template-parts/footer/site', 'info' );
-                    ?>
-            </div><!-- .wrap -->
+        <div class="wrap">
+                <?php
+                    get_template_part( 'template-parts/footer/site', 'info' );
+                ?>
+        </div><!-- .wrap -->
     </footer><!-- #colophon -->
 </div><!-- .site-content-contain -->
--- wp-content/themes/coezion2/header.php
+++ wp-content/themes/coezion2/header.php
@@ -3,8 +3,26 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         <?php wp_head(); ?>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+        <script>
+            var $ = jQuery;
+            $(document).ready(function(){
+                $('.mega-menu-wrap .mega-menu-toggle').click(function(){
+                    var menu_button = $("#mega-menu-wrap-Top .mega-menu-toggle + #mega-menu-Top");
+                    menu_button.toggle("slow");                    
+                });
+                
+                var lien_menu = $('#mega-menu-wrap-Top #mega-menu-Top li.mega-menu-item-has-children > a.mega-menu-link');
+                lien_menu.after("<span class='arrow'><i class='fas fa-caret-down'></i></span>");
+                
+                var row_menu = $('#mega-menu-wrap-Top #mega-menu-Top li.mega-menu-item-has-children');
+                row_menu.click(function(){
+                    var show_sub_menu = $(this).find('ul.mega-sub-menu');
+                    show_sub_menu.toggle("slow");
+                })
+            })
+        </script>
     </head>
     <body>
         <header>
